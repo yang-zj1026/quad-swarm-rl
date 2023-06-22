b2d1506d25b59a52352b104512d01b2eb00f1e60
yang-zj1026 <victoryang1026@gmail.com>
2023-06-22 15:50:24 -0700

    Update training script for new position reward

---
+++ swarm_rl/runs/obstacles/new_pos_rew.py
@@ -0,0 +1,24 @@
+from sample_factory.launcher.run_description import Experiment, ParamGrid, RunDescription
+from swarm_rl.runs.obstacles.quad_obstacle_baseline import QUAD_BASELINE_CLI_8
+
+_params = ParamGrid(
+    [
+        ("seed", [0000, 1111]),
+        ("quads_pos_rew_coeff", [20, 50, 100, 200])
+    ]
+)
+
+OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
+    ' --num_workers=36 --num_envs_per_worker=4 --quads_num_agents=8 --quads_use_downwash=True '
+    '--quads_neighbor_visible_num=6 --quads_neighbor_obs_type=pos_vel --quads_encoder_type=attention '
+    '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones '
+    '--wandb_group=renew_pos_reward'
+)
+
+_experiment = Experiment(
+    "renew_pos_reward",
+    OBSTACLE_MODEL_CLI,
+    _params.generate_params(randomize=False),
+)
+
+RUN_DESCRIPTION = RunDescription("obstacles_multi", experiments=[_experiment])
