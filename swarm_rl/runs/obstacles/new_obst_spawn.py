5fee1bb83ebb3ac5b341656603f643d2c8580bc7
yang-zj1026 <victoryang1026@gmail.com>
2023-06-25 11:54:00 -0700

    Add runnning script for obstacles spawning with noises

---
+++ swarm_rl/runs/obstacles/new_obst_spawn.py
@@ -0,0 +1,24 @@
+from sample_factory.launcher.run_description import Experiment, ParamGrid, RunDescription
+from swarm_rl.runs.obstacles.quad_obstacle_baseline import QUAD_BASELINE_CLI_8
+
+_params = ParamGrid(
+    [
+        ("seed", [0000, 1111, 2222, 3333]),
+    ]
+)
+
+OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
+    ' --num_workers=36 --num_envs_per_worker=4 '
+    '--quads_neighbor_visible_num=6 --quads_neighbor_obs_type=pos_vel --quads_encoder_type=attention '
+    '--quads_use_obst_min_gap=True --quads_obst_size=0.4 '
+    '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones '
+    '--wandb_group=new_obst_spawn'
+)
+
+_experiment = Experiment(
+    "new_obst_spawn",
+    OBSTACLE_MODEL_CLI,
+    _params.generate_params(randomize=False),
+)
+
+RUN_DESCRIPTION = RunDescription("obstacles_multi", experiments=[_experiment])
