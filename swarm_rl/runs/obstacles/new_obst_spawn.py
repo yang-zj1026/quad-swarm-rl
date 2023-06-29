7164329a73184fb43b8e94075a4debdf331dc831
yang-zj1026 <victoryang1026@gmail.com>
2023-06-28 18:02:55 -0700

    Bug fix in running script

--- swarm_rl/runs/obstacles/new_obst_spawn.py
+++ swarm_rl/runs/obstacles/new_obst_spawn.py
@@ -10,7 +10,7 @@ _params = ParamGrid(
 OBSTACLE_MODEL_CLI = QUAD_BASELINE_CLI_8 + (
     ' --num_workers=36 --num_envs_per_worker=4 '
     '--quads_neighbor_visible_num=6 --quads_neighbor_obs_type=pos_vel --quads_encoder_type=attention '
-    '--quads_use_obst_min_gap=True --quads_obst_size=0.4 '
+    '--quads_use_obst_min_gap=True --quads_obst_min_gap=0.4 '
     '--with_wandb=True --wandb_project=Quad-Swarm-RL --wandb_user=multi-drones '
     '--wandb_group=new_obst_spawn'
 )
